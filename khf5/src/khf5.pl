70fb77e88e089c84b1aa135feaa506160d1a90a4
Dancs Balázs <100741583+dancsb@users.noreply.github.com>
2023-10-30 23:18:47 +0100

    comments

--- khf5/src/khf5.pl
+++ khf5/src/khf5.pl
@@ -24,7 +24,7 @@ get_tent(Fx-Fy, n, Tx-Ty) :- Fx1 is Fx - 1, Tx-Ty = Fx1 - Fy. % Észak
 get_tent(Fx-Fy, s, Tx-Ty) :- Fx1 is Fx + 1, Tx-Ty = Fx1 - Fy. % Dél
 get_tent(Fx-Fy, w, Tx-Ty) :- Fy1 is Fy - 1, Tx-Ty = Fx - Fy1. % Nyugat
 
-% Egy adott fa iránylistájának meghatározása. Egy adott irány akkor megfelelő, ha az általa mutatott sátor nem lóg le a pályáról, és nem létzeik, olyan fa.
+% Egy adott fa iránylistájának meghatározása. Egy adott irány akkor megfelelő, ha az általa mutatott sátor nem lóg le a pályáról, és nem létezik olyan fa.
 get_directions(N-M, Fs, Fx-Fy, Dir) :-
     (get_tent(Fx-Fy, e, Txe-Tye), Tye =< M, nonmember(Txe-Tye, Fs) -> EDir = [e]; EDir = []), % Kelet
     (get_tent(Fx-Fy, n, Txn-Tyn), Txn >= 1, nonmember(Txn-Tyn, Fs) -> append(EDir, [n], NDir); NDir = EDir), % Észak
@@ -32,7 +32,7 @@ get_directions(N-M, Fs, Fx-Fy, Dir) :-
     (get_tent(Fx-Fy, w, Txw-Tyw), Tyw >= 1, nonmember(Txw-Tyw, Fs) -> append(SDir, [w], WDir); WDir = SDir), % Nyugat
     Dir = WDir.
 
-% Elfogytaka a fák
+% Elfogytak a fák
 iranylistak_core(_, _, [], []).
 
 % Iránylisták meghatározása rekurzívan a fák alapján
@@ -45,7 +45,7 @@ iranylistak(N-M, Fs, ILs) :-
     (nonmember([], Dir) -> ILs = Dir; ILs = []), % Üres iránylisták ellenőrzése
     !. % cut
 
-% Adott fa meglévő iránylistájának újrartékelése, szűkítése. Egy adott irány akkor maradhat, ha eddig is volt és az általa mutatott sátor X ésvagy Y koordinátájának a deltája nagyobb mint 1 az I-edik fa sátorjához képest.
+% Adott fa meglévő iránylistájának újrartékelése, szűkítése. Egy adott irány akkor maradhat, ha eddig is volt, és az általa mutatott sátor X ésvagy Y koordinátájának a deltája nagyobb mint 1 az I-edik fa sátorához képest.
 reevaluate_directions(Fx-Fy, Ix-Iy, PrevDir, Dir) :-
     (member(e, PrevDir), get_tent(Fx-Fy, e, Txe-Tye), Dxe is Ix - Txe, Dye is Iy - Tye, (abs(Dxe) > 1; abs(Dye) > 1) -> EDir = [e]; EDir = []), % Kelet
     (member(n, PrevDir), get_tent(Fx-Fy, n, Txn-Tyn), Dxn is Ix - Txn, Dyn is Iy - Tyn, (abs(Dxn) > 1; abs(Dyn) > 1) -> append(EDir, [n], NDir); NDir = EDir), % Észak
@@ -53,7 +53,7 @@ reevaluate_directions(Fx-Fy, Ix-Iy, PrevDir, Dir) :-
     (member(w, PrevDir), get_tent(Fx-Fy, w, Txw-Tyw), Dxw is Ix - Txw, Dyw is Iy - Tyw, (abs(Dxw) > 1; abs(Dyw) > 1) -> append(SDir, [w], WDir); WDir = SDir), % Nyugat
     Dir = WDir.
 
-% Elfogytaka az iránylisták
+% Elfogytak az iránylisták
 sator_szukites_core([], _, _, [], []).
 
 % Iránylisták szűkítése rekurzívan
